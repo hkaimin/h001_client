ede6029a4c817b3d146e946aa11ca4584b1d0e95
黄凯敏 <huangkaimin@huangkaimindeMacBook-Air.local>
2021-11-20 15:12:29 +0800

    egretia sdk

---
+++ libs/lzma/lzma.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Exin
+ */
+declare module LZMA{
+    function decompress(value:number[],cb:Function,pro:Function):any;
+    function compress(value:number[],cb:Function,pro:Function):any;
+}
